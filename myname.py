e8b59c94d7cd32d5e0dd46cbec5b7eb0171b53f0
ginibh <ragini.bhargava31@gmail.com>
2018-10-16 11:17:59 -0400

    updated file for additional code

--- myname.py
+++ myname.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 name="Ragini" #giving variable a name
-print('My name:',name) 
+print('My name:', name)
+color="black" #adding color
+print('My favorite color:', color)
+activity="Shopping" # added an activity
+print("My favorite activity:", activity)
+animal="tiger" #added code for my fav animal
+print("My favorite animal:", animal) 
