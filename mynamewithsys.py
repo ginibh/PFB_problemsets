b13378d01abc39fe80f82f780c4a154ffd3e0b59
ginibh <ragini.bhargava31@gmail.com>
2018-10-16 11:46:46 -0400

    same file with sys

---
+++ mynamewithsys.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import sys
+name = sys.argv[1] #name Ragini
+color = sys.argv[2] #color Black
+activity = sys.argv[3] #activity Shopping
+animal = sys.argv[4] #animal Tiger
+print('My name is:', name + '\n' + 'My favorite color is:',color + '\n'+ 'My favorite activity is:', activity + '\n' + 'My favorite animal is:', animal)
