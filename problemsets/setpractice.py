39be6ec5a936a3456ecd93ce46d62e8843497040
ginibh <ragini.bhargava31@gmail.com>
2018-10-17 21:30:08 -0400

    add set

---
+++ problemsets/setpractice.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+A ={3, 14, 15, 9, 26, 5, 35, 9}
+B={60, 22, 14, 0, 9}
+set_intersection=A & B
+print(set_intersection)
+set_difference= A - B
+print(set_difference)
+set_union= A|B
+print(set_union)
+set_symmetrical_difference=A ^ B
+print(set_symmetrical_difference)
